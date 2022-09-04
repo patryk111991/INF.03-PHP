145f91fd1cee0a76e11f52007c3b9fed8ca5c00e
Patryk Podgórny <59233058+patryk111991@users.noreply.github.com>
2022-09-04 09:16:13 +0200

    Add files via upload

---
+++ 46_Przykład_46_2_str_361.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Przykład 46.2 - Struktura języka PHP</title>
+	</head>
+
+	<body>
+		<?php
+			echo "Przykłady komentarzy";
+			
+			//Komentarz jednowierszowy
+			
+			/*Komentarz skaładający się
+			z wielu
+			wierszy*/
+			
+			# Komentarz jednowierszowy Uniksowy
+		?>
+	</body>
+
+</html>
